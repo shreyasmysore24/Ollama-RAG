b40dc63d6f27d7b7a3cf5e1e1537cc059412bd11
Shreyas S R <152903031+shreyasmysore24@users.noreply.github.com>
2025-02-08 13:02:13 +0530

    Delete check.py

--- check.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-response = requests.get("https://brainlox.com/courses/category/technical")
-print(f"Status Code: {response.status_code}")  # Should be 200
-print(f"Content Length: {len(response.text)}")  # Should be > 0
